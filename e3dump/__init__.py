530350d54f9e56bdf3372b6e655a4a26fc892360
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-23 15:08:33 +0800

    Delete e3dump directory

--- e3dump/__init__.py
+++
@@ -1,3 +0,0 @@
-"""e3dump - dump your NCTU e3 course materials by one click"""
-
-__version__ = '1.0.0'
--- e3dump/__main__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from e3dump.cli import run
-
-
-if __name__ == '__main__':
-    run()
